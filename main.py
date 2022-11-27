88fdee3c6b012fc8bd25559a522b68d04582b594
2pokek <alkuzmich6479@gmail.com>
2022-11-28 01:16:13 +0300

    Almost

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 from gtts import gTTS
+from art import tprint
 import pdfplumber
 from pathlib import Path
 
 def pdf_to_mp3(file_path='test.pdf',language='en'):
     if Path(file_path).is_file() and Path(file_path).suffix == '.pdf':
-        return 'file exists'
+        print(f'[+] Original file: {Path(file_path).name}')
+        print('[+] Processing...')
+
+        with pdfplumber.PDF(open(file=file_path, mode ='rb')) as pdf:
+            pages = [page.extract_text() for page in pdf.pages]
+        text = ''.join(pages)
+
+        # with open('text1.txt','w') as file:
+        #     file.write(text)
+
+        text = text.replace('/n','')
+
+        # with open('text2.txt','w') as file:
+        #     file.write(text)
+        my_audio =gTTS(text=text, lang=language ,slow=False)
+        file_name = Path(file_path)
+        my_audio.save(f"{file_name}.mp3")
+        return f'[+] {file_name}.mp3 saved successfully!/n Have a good day!'
     else:
         return 'ERROR'
 
 def main():
-    print(pdf_to_mp3(file_path="/"))
+    tprint('PDF>>TO>>MP3')
+    file_path = input("Enter a file's path: ")
+    language = input("Choose Language: ")
+    print(pdf_to_mp3(file_path=file_path,language=language))
 
 if __name__ == '__main__':
     main()
